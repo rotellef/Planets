647141a448e718de18e3090f744800f943ee1027
rotellef <robert.tellefsen@gmail.com>
2018-12-11 14:23:22 +0100

    Add files via upload

---
+++ Planets/Domain/Ship.cs
@@ -0,0 +1,41 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planets.Domain
+{
+	public class Ship
+	{
+		public string Name { get; set; }
+
+		[JsonConverter(typeof(StringEnumConverter))]
+		public ShipClass Class { get; set; }
+		[JsonConverter(typeof(StringEnumConverter))]
+		public ShipClass TargetPrimary { get; set; }
+		[JsonConverter(typeof(StringEnumConverter))]
+		public ShipClass TargetSecondary { get; set; }
+		public int Init { get; set; }
+		public int Agility { get; set; }
+		public int Weaponspeed { get; set; }
+		public int Guns { get; set; }
+		public int Power { get; set; }
+		public int Armour { get; set; }
+		public int Resistance { get; set; }
+		public int Metal { get; set; }
+		public int Crystal { get; set; }
+		public int Fuel { get; set; }
+		public int ETA { get; set; }
+
+		[JsonConverter(typeof(StringEnumConverter))]
+		public ShipType Type { get; set; }
+
+		public override string ToString() =>
+			$"{Name}, {Class}, {TargetPrimary}, {TargetSecondary}, {Init}, {Type}";
+	}
+	public enum ShipType { Classic, Special, Cloaked, EMP, PDS }
+	public enum ShipClass { FI,	CO,	FR,	DE,	CR,	BS,	NA }
+}
---
+++ Planets/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Planets
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+		public MainWindow()
+		{
+
+			foreach (var item in Ships.ShipStats)
+			{
+				logger.Debug("{0}", item.Value);
+			}
+			InitializeComponent();
+		}
+	}
+}
